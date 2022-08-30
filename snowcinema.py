7c8a9fdf263f2dfafe768672430e5e440165bb87
10rino <112349945+10rino@users.noreply.github.com>
2022-08-30 19:08:06 +0430

    new

--- snowcinema.py
+++ snowcinema.py
@@ -1,43 +1,59 @@
-from flask import Flask
-from flask import request
-from flask import Response
-import requests
- 
-TOKEN = "5666296465:AAFCC5I8QN6pWhad0ZoprGFjFyIVG1CbfWM"
-app = Flask(__name__)
- 
-def parse_message(message):
-    print("message-->",message)
-    chat_id = message['message']['chat']['id']
-    txt = message['message']['text']
-    print("chat_id-->", chat_id)
-    print("txt-->", txt)
-    return chat_id,txt
- 
-def tel_send_message(chat_id, text):
-    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-    payload = {
-                'chat_id': chat_id,
-                'text': text
-                }
-   
-    r = requests.post(url,json=payload)
-    return r
- 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        msg = request.get_json()
-       
-        chat_id,txt = parse_message(msg)
-        if txt == "/ozviat":
-            tel_send_message(chat_id,"https://t.me/+fbO6I1VKJzZmNjE0")
-        elif txt == "/darkhast" :
-            tel_send_message(chat_id,'اسم و فیلم سریال درخواستی خود را ارسال کنید')
-       
-        return Response('ok', status=200)
-    else:
-        return "<h1>Welcome!</h1>"
- 
-if __name__ == '__main__':
-   app.run(debug=True)
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.updater import Updater
+from telegram.ext.dispatcher import Dispatcher
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.bot import Bot
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.update import Update
+
+
+updater = Updater("5666296465:AAFCC5I8QN6pWhad0ZoprGFjFyIVG1CbfWM",
+                  use_context=True)
+
+
+dispatcher: Dispatcher = updater.dispatcher
+
+
+def start(update: Update, context: CallbackContext):
+    """
+    the callback for handling start command
+    """
+
+    bot: Bot = context.bot
+    bot.send_message(chat_id=update.effective_chat.id,
+                     text="به ربات تلگرامی چنل @snowcinema خوش آمدید\n\n\nاگر پیشنهاد انتقاد یا درخواستی دارید لطفا اعلام کنید")
+
+dispatcher.add_handler(
+    CommandHandler("start", start))
+
+def echo(update: Update, context: CallbackContext):
+    """
+    the callback for handling echo command
+    """
+
+    bot: Bot = context.bot
+    bot.send_message(chat_id=update.effective_chat.id,
+                     text="https://t.me/+fbO6I1VKJzZmNjE0")
+
+dispatcher.add_handler(
+    CommandHandler("ozviat", echo))
+
+def echo(update: Update, context: CallbackContext):
+    """
+    the callback for handling echo command
+    """
+
+    bot: Bot = context.bot
+    bot.send_message(chat_id=update.effective_chat.id,
+                     text="لطفا اسم فیلم و درخواستی خود را بنویسید")
+
+dispatcher.add_handler(
+    CommandHandler("darkhast", echo))
+
+
+
+
+
+
+updater.start_polling()
